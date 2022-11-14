fd40bdfb55b152d59cd002531161c39927e83dac
Pedro Silva <87133537+pedroD630@users.noreply.github.com>
2022-11-14 06:55:52 -0300

    Add files via upload

---
+++ script/myscript.js
@@ -0,0 +1,9 @@
+//definir tamanho da tela e das divs
+let width = window.innerWidth;
+let tamDivs = width/3;
+
+let da = document.getElementById("div1");
+let db = document.getElementById("div2");
+let dc = document.getElementById("div3");
+
+da.style.color = 'red';
