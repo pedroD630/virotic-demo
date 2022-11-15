1cb5744da010cbe265d356cf55aa13a5177a00c9
Pedro Silva <87133537+pedroD630@users.noreply.github.com>
2022-11-15 09:06:01 -0300

    Consertei bug do scroll do navegador

--- script/myscript.js
+++ script/myscript.js
@@ -1,10 +1,1 @@
-//definir tamanho da tela e das divs
-let width = window.innerWidth;
-let tamDivs = width/3;
-
-let da = document.getElementById("div1");
-let db = document.getElementById("div2");
-let dc = document.getElementById("div3");
-
-da.style.color = 'red';
 
